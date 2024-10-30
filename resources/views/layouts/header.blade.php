e99d0565550cf45eb9a09b3b4fb06f5c2081066c
main125 <tasyameilap2006@gmail.com>
2024-10-30 17:16:14 +0900

    Signed-off-by: main125 <tasyameilap2006@gmail.com>

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,24 +5,14 @@
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
+        <a href="index3.html" class="nav-link">Hallo, selamat pagi!</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
+        <a href="#" class="nav-link"></a>
       </li>
     </ul>
 
-    <!-- SEARCH FORM -->
-    <form class="form-inline ml-3">
-      <div class="input-group input-group-sm">
-        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-          <button class="btn btn-navbar" type="submit">
-            <i class="fa fa-search"></i>
-          </button>
-        </div>
-      </div>
-    </form>
+   
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
@@ -107,13 +97,40 @@
             <i class="fa fa-file mr-2"></i> 3 new reports
             <span class="float-right text-muted text-sm">2 days</span>
           </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-        </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i
-            class="fa fa-th-large"></i></a>
+
+      
+      <li class="nav-item dropdown no-arrow"> 
+        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <!-- <span class="mr-2 d-none d-lg-block text-black small">Administratore></span>
+          <br> --> 
+        <img class="mr-2 img-profile rounded-circle" scr="https://jombangwifi.id/img/undraw profile.svg"> 
+      <div> ==$0
+        <span class="d-none d-lg-block small">Administrator
+        </span>
+        <span class="d-none d-lg-block small">Admin</span>
+      </div>
+      </a>
+      <div class="dropdown-menu dropdown-menu-right shadow animated 
+      --grow-in" aria-labelledby="userDropdown"> 
+      <a class="dropdown-item" href="user/profile">
+        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400">
+        </i>
+        "profile"
+      </a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal"> 
+        <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+        "logout"
+      </a>
+      </div>
       </li>
+
+          
+
+
+      <li class="nav-item d-none d-sm-inline-block">
+      <a href="#" class="nav-link">user</a>
+       </li>
     </ul>
   </nav>
