53950c31611ac636cbdab8c43a75c1f2a729f6fd
main125 <tasyameilap2006@gmail.com>
2024-10-31 11:30:09 +0900

    Signed-off-by: main125 <tasyameilap2006@gmail.com>

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -17,63 +17,7 @@
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
       <!-- Messages Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="fa fa-comments-o"></i>
-          <span class="badge badge-danger navbar-badge">3</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="{{ asset('/dist/img/user1-128x128.jpg') }}" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Brad Diesel
-                  <span class="float-right text-sm text-danger"><i class="fa fa-star"></i></span>
-                </h3>
-                <p class="text-sm">Call me whenever you can...</p>
-                <p class="text-sm text-muted"><i class="fa fa-clock-o mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="{{ asset('/dist/img/user8-128x128.jpg') }}" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  John Pierce
-                  <span class="float-right text-sm text-muted"><i class="fa fa-star"></i></span>
-                </h3>
-                <p class="text-sm">I got your message bro</p>
-                <p class="text-sm text-muted"><i class="fa fa-clock-o mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="/dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Nora Silvester
-                  <span class="float-right text-sm text-warning"><i class="fa fa-star"></i></span>
-                </h3>
-                <p class="text-sm">The subject goes here</p>
-                <p class="text-sm text-muted"><i class="fa fa-clock-o mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-        </div>
-      </li>
+      
       <!-- Notifications Dropdown Menu -->
       <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
@@ -114,8 +58,7 @@
       <div class="dropdown-menu dropdown-menu-right shadow animated 
       --grow-in" aria-labelledby="userDropdown"> 
       <a class="dropdown-item" href="user/profile">
-        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400">
-        </i>
+        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
         "profile"
       </a>
       <div class="dropdown-divider"></div>
@@ -125,9 +68,6 @@
       </a>
       </div>
       </li>
-
-          
-
 
       <li class="nav-item d-none d-sm-inline-block">
       <a href="#" class="nav-link">user</a>
--- resources/views/pengguna/index.blade.php
+++ resources/views/pengguna/index.blade.php
@@ -9,7 +9,33 @@
     </div><!-- /.row -->
 @endsection
 @section('content')
-    <div class="container">
+    <div class="row my-3">
+        <div class="col-sm-4 my-2">
+        <div class="card shadow h-100 py-2">
+        <div class="card-body"> ==$0
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-uppercase mb-1">
+                        "Sekolah"
+                    </div> 
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                        "25"
+                    </div> 
+                    </div>
+        
+             <div class="col-auto"> ==$0
+                <img scr="https://jombangwifi.id/img/icons/user.svg" alt="icon-users">
+            </div>
+         </div>
+       </div>
+     </div>
+   </div>
+
+   
+         
+       
+
+     <div class="container">
         <h2>Selamat Datang Pengguna</h2>
         <form action="/logout" method="post">
             @csrf
