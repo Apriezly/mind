f341757d23eb6c7316d9b7d0956ec48e4a0103cb
Apriezly <sitiawwalinaauliawati@gmail.com>
2024-10-31 11:38:00 +0700

    Signed-off-by: Apriezly <sitiawwalinaauliawati@gmail.com>

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,42 +1,43 @@
-<aside class="main-sidebar sidebar-white-primary">
+<aside class="main-sidebar elevation-2" style="background:#19A177; border-radius: 0px 32px 32px 0px;">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link">
-      <img src="{{ asset('/element/iconsite.png') }}" alt="AdminLTE Logo">
-      <span class="brand-text font-weight-light">Mind</span>
+    <nav class="pt-5">
+    <a class="px-5">
+      <img src="{{ asset('/element/mind-max.png') }}" alt="Mind">
     </a>
+    </nav>
 
     <!-- Sidebar -->
     <div class="sidebar">
-      <nav class="mt-2">
+      <nav class="pt-5">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
-            <a href="{{ url('/beranda-p') }}" class="nav-link active">
-              <i class="fa fa-dashboard"></i>
-              <p>Beranda</p>
+            <a href="{{ url('/beranda-p') }}" class="nav-link" style="background:#D1F4F0; border16px;">
+            <img src="{{ asset('/element/beranda-active.png') }}">
+              <span style="color:#02432f">&emsp;Beranda</span>
             </a>
           </li>
           <li class="nav-item">
             <a href="{{ url('/kalender') }}" class="nav-link">
-            <i class="fa fa-calendar-o"></i>
-              <p>Kalender</p>
+            <img src="{{ asset('/element/kalender.png') }}" style="width:25px">
+            <span style="color:#D1F4F0">&emsp;Kalender</span>
             </a>
           </li>
           <li class="nav-item">
             <a href="{{ url('/pengingat') }}" class="nav-link">
-              <i class="fa fa-clock-o"></i>
-              <p>Pengingat</p>
+            <img src="{{ asset('/element/pengingat.png') }}" style="width:25px">
+            <span style="color:#D1F4F0">&emsp;Pengingat</span>
             </a>
           </li>
           <li class="nav-item">
             <a href="{{ url('/laporan') }}" class="nav-link">
-              <i class="fa fa-file-o"></i>
-              <p>Laporan</p>
+            <img src="{{ asset('/element/laporan.png') }}">
+            <span style="color:#D1F4F0">&emsp;Laporan</span>
             </a>
           </li>
           <li class="nav-item">
             <a href="{{ url('/bantuan') }}" class="nav-link">
-              <i class="fa fa-question-circle"></i>
-              <p>Bantuan</p>
+            <img src="{{ asset('/element/bantuan.png') }}">
+            <span style="color:#D1F4F0">&emsp;Bantuan</span>
             </a>
           </li>
         </ul>
@@ -44,5 +45,8 @@
       <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
-
+  <!-- sidebar footer -->
+   <div class="sidebar-footer">
+    <p>bismillah</p>
+   </div>
   </aside>
