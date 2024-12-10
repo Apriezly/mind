f857777a121586217b2664630e8f2b81ade47f23
main125 <tasyameilap2006@gmail.com>
2024-12-10 16:29:18 +0900

    Signed-off-by: main125 <tasyameilap2006@gmail.com>

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,8 +13,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
 
   <!-- Data tabel -->
-  <!-- <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" /> -->
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" />
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="{{ asset('/lte/plugins/font-awesome/css/font-awesome.min.css') }}">
   <!-- Theme style -->
@@ -22,11 +21,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link rel="stylesheet" href="{{ asset('/app.css') }}">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <!-- Pesan yg di login -->
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
   <!-- <link rel="stylesheet" href="{{ asset('multiple/dist/virtual-select.min.css')}}"> -->
-  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@3.1.0/dist/css/multi-select-tag.css"> -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@3.1.0/dist/css/multi-select-tag.css">
    <!-- Latest compiled and minified CSS -->
   <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css"> -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+
+
 
   <!-- icon site -->
   <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/element/iconbg.png') }}">
@@ -62,8 +65,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
     </div>
     <!-- /.content -->
   </div>
-
-  @include('layouts/footer')
   <!-- /.content-wrapper -->
 </div>
 <!-- ./wrapper -->
@@ -71,22 +72,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- REQUIRED SCRIPTS -->
 <!-- jQuery -->
 <script src="{{ asset('/lte/plugins/jquery/jquery.min.js')}}"></script>
-<!-- chosen -->
-<script src="https://cdn.jsdelivr.net/npm/chosen-js@1.8.7/chosen.jquery.min.js" integrity="sha256-c4gVE6fn+JRKMRvqjoDp+tlG4laudNYrXI1GncbfAYY=" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/chosen-js@1.8.7/chosen.min.css" integrity="sha256-EH/CzgoJbNED+gZgymswsIOrM9XhIbdSJ6Hwro09WE4=" crossorigin="anonymous">
 <!-- Bootstrap 4 -->
 <script src="{{ asset('/lte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <script src="{{ asset('/lte/dist/js/adminlte.min.js')}}"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <!-- script buatan sendiri -->
 <script src="{{ asset('/script.js') }}"></script>
-<!-- <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script> -->
+<script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
 <!-- yg ada di login -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-@yield('script')
-
-
 <!-- <script src="{{ asset('multiple/dist/virtual-select.min.js')}}"></script> -->
 <!-- <script>
         VirtualSelect.init({
@@ -99,6 +92,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     $('select').selectpicker();
 </script> -->
 <script>
+        //message with toastr
         @if(session()->has('success'))
         
             toastr.success('{{ session('success') }}', 'BERHASIL!'); 
